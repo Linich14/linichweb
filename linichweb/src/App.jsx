b0f509458cd6d49bcf569fde35de7a8e4da028a1
linich <jorge_soto_1999@hotmail.com>
2023-12-02 07:05:08 -0300

    Setup de Proyecto

---
+++ linichweb/src/App.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react'
+
+
+export default function App() {
+ 
+
+  return (
+    <>
+          <div>
+              <h1 >Hola aaaaaaaaaaaaa</h1>
+              
+          </div>
+    </>
+  )
+}
+
+
